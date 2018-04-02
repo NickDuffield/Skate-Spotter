35f0b1efabe3c32f5c567df4f709d7a9ced8ce7a
Nick Duffield <hi.nickduffield@gmail.com>
2018-04-02 07:01:38 +0100

    Radio button selection values set for submit

--- public/scripts/firebase/spots-push.js
+++ public/scripts/firebase/spots-push.js
@@ -2,19 +2,43 @@
 var testPublicSpotsRef = firebase.database().ref('public-spots');
 
 // listen for form submit
-document.getElementById('upload-form').addEventListener('submit', submitForm);
+document.getElementById('upload-form').addEventListener('submit', uploadSpot);
 
-function submitForm(e) {
+function uploadSpot(e) {
   e.preventDefault();
 
   // get values
-  var coords = {lat:52.28, lng: 0.116211};
-  var iconImg = "assets/marker-street.svg";
-  var imgUrl = "https://firebasestorage.googleapis.com/v0/b/skate-spotter-51441.appspot.com/o/starter-images%2F1408882415497.jpg?alt=media&token=906603cd-2250-4f3f-8314-98f727787ed5";
+  var coords = {lat:52.28, lng: 0.116211}; // @TODO 2. need to find the best way to capture users position
+  var iconImg;
+  var imgUrl = "https://firebasestorage.googleapis.com/v0/b/skate-spotter-51441.appspot.com/o/starter-images%2F1408882415497.jpg?alt=media&token=906603cd-2250-4f3f-8314-98f727787ed5"; // @TODO 1. need to refactor code to set this to select file, capture download url then set message
   var spotName = getInputVal('spot-name');
 
+  // capture radio button group
+  var spotRadios = document.getElementsByName('spotType');
+  // loop through radios to capture value
+  for (var i = 0, length = spotRadios.length; i < length; i++) {
+    if (spotRadios[i].checked) {
+      // selected spotType value
+      var spotType = spotRadios[i].value;
+      // set iconImg variable to selected value
+      iconImg = 'assets/marker-'+spotType+'.svg';
+      // only one radio can be logically checked, don't check the rest
+      break;
+    }
+  }
+
   saveSpot(coords, iconImg, imgUrl, spotName);
-  //console.log(123);
+
+  // show confimation message
+  var uploadConfirmation = document.getElementById('upload-confirmation-message');
+  uploadConfirmation.style.display = 'block'
+
+  // @TODO this needs to happen after the full upload is complete
+  // hide alert after 3 seconds
+  setTimeout(function(){
+    uploadConfirmation.style.display = 'none'
+  }, 3000);
+
 }
 
 // function used to capture form input values
